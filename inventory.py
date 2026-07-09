232a805cc661271b005762229610c707ac8b98df
Earl <earljamabo6@gmail.com>
2026-07-09 18:33:36 +0100

    Added the inventory module containing a list of dictionary items

---
+++ inventory.py
@@ -0,0 +1,34 @@
+items = [
+    {
+        "item_name": "Cotton_wool",
+        "quantity": 30,
+        "price": 3.00
+    },
+
+    {
+        "item_name": "Disposable_syringe",
+        "quantity": 20,
+        "price": 4.50
+        
+    },
+    {
+        "item_name": "Painkillers",
+        "quantity": 15,
+        "price": 5.00
+        
+    },
+    {
+        "item_name": "Q-tips",
+        "quantity": 25,
+        "price": 0.45
+        
+    },
+    {
+        "item_name": "Wipes",
+        "quantity": 10,
+        "price": 2.50
+        
+    },
+
+
+]
